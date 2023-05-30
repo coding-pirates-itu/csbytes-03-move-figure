5746067171fba7aeeded078d2b061c76769a2dc2
mc0re <mnikitin0@gmail.com>
2023-05-30 20:40:06 +0200

    Move a character

---
+++ MoveCharacter/Program.cs
@@ -0,0 +1,53 @@
+﻿var character = new[]
+{
+    "  (°°)  ",
+    "==╡██╞==",
+    "  └║║┘  ",
+};
+
+var isExit = false;
+var x = 0;
+var y = 0;
+var charWidth = character[0].Length;
+var charHeight = character.Length;
+Console.CursorVisible = false;
+
+while (!isExit)
+{
+    Console.Clear();
+    var windowWidth = Console.BufferWidth;
+    var windowHeight = Console.BufferHeight;
+
+    for (var lineIndex = 0; lineIndex < charHeight; lineIndex++)
+    {
+        var line = character[lineIndex];
+        Console.SetCursorPosition(x, y + lineIndex);
+        Console.Write(line);
+    }
+
+    var ch = Console.ReadKey(true);
+
+    switch (ch.Key)
+    {
+        case ConsoleKey.Q:
+        case ConsoleKey.Escape:
+            isExit = true;
+            break;
+
+        case ConsoleKey.UpArrow when y > 0:
+            y--;
+            break;
+
+        case ConsoleKey.DownArrow when y + charHeight < windowHeight:
+            y++;
+            break;
+
+        case ConsoleKey.LeftArrow when x > 0:
+            x--;
+            break;
+
+        case ConsoleKey.RightArrow when x + charWidth < windowWidth:
+            x++;
+            break;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# csbytes-03-move-figure
-C# byte #3 - Move a figure on the screen
+# C# byte #3 - Move a figure on the screen
+
+## Pseudo graphics
+
+Use (this table)[https://devblogs.microsoft.com/wp-content/uploads/sites/33/2019/02/5-Buffers-extended-ascii-codepage.png]
